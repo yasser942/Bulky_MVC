3490b30c230c1ff1cc3fbd11004f4b8a79adc9ff
kingn <yasseralhassan942@gmail.com>
2023-09-29 12:47:57 +0300

    controller edited

--- BulkyWeb/Controllers/HomeController.cs
+++ BulkyWeb/Controllers/HomeController.cs
@@ -15,7 +15,9 @@ namespace BulkyWeb.Controllers
 
         public IActionResult Index()
         {
+            
             return View();
+            
         }
 
         public IActionResult Privacy()
