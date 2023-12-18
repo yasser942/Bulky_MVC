a912326c3ae9ada75a917e829bd661f0514b3030
kingn <yasseralhassan942@gmail.com>
2023-12-18 21:34:56 +0300

    Get Category Details to Edit

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -38,5 +38,19 @@ namespace BulkyWeb.Controllers
             return View(obj);
            
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id==0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Categories.Find(id);
+            if(obj==null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
     }
 }
