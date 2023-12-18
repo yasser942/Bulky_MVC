752b0d926d3fd7d28ed257139a10118ac2d11492
kingn <yasseralhassan942@gmail.com>
2023-12-18 13:27:03 +0300

    Server Side Validations

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -7,10 +7,12 @@ public class Category
     [ Key ]
     public int  Id { get; set; }
     [Required]
+    [MaxLength(50)]
     [Display(Name="Category Name")] 
     public string Name { get; set; }
     [Required]
     [Display(Name="Display Order")]
+    [Range(1,100,ErrorMessage="Display Order for category must be between 1 and 100")]
     public int DisplayOrder { get; set; }
     
 }
