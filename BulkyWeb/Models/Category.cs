a4ba7d004a1a7f0e27bf4d5499bdcd6b0bae89d1
kingn <yasseralhassan942@gmail.com>
2023-12-18 13:11:07 +0300

    Input Tag Helpers

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,6 +1,16 @@
-﻿namespace BulkyWeb.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models;
 
 public class Category
-{
+{   
+    [ Key ]
+    public int  Id { get; set; }
+    [Required]
+    [Display(Name="Category Name")] 
+    public string Name { get; set; }
+    [Required]
+    [Display(Name="Display Order")]
+    public int DisplayOrder { get; set; }
     
 }
