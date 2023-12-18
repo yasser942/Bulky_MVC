c3ae03d56061d63f129bd95256bfa0c6e4eebb0c
kingn <yasseralhassan942@gmail.com>
2023-12-18 21:03:14 +0300

    Custom Validations

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -23,6 +23,12 @@ namespace BulkyWeb.Controllers
         [HttpPost]
     public IActionResult Create(Category obj)
         {
+            if (obj.Name==obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name","Name and Display Order cannot be the same");
+
+            }
+
             if(ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
